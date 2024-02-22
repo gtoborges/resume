47a53d73d0c533207ba95c513b2c907f8fc69010
Augusto <gtoborges@gmail.com>
2024-02-22 20:32:52 -0300

    recreate project with nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup>
+import resume_config from '../assets/config/resume_config.json'
+console.log(resume_config)
+</script>
+<template>
+  <nav style="display:flex; gap:10px;">
+    <template v-for="language of Object.keys(resume_config.languages)">
+      <NuxtLink :to="`/${language}`">{{ resume_config.languages[language] }}</NuxtLink>
+    </template>
+  </nav>
+  <slot></slot>
+</template>
