73b928cb4b464667c0acb9054ec0200b663e11fa
Augusto <gtoborges@gmail.com>
2024-02-28 21:27:23 -0300

    comment language select

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,26 @@
 <script setup>
 const { resume_config, lang } = useResumeConfig()
 
-const language = ref(lang)
-const selectLanguage = (e) => {
-  navigateTo({ path: `/${e.target.value}`})
-  language.value = e.target.value
-}
+// const language = ref(lang)
+// const selectLanguage = (e) => {
+//   navigateTo({ path: `/${e.target.value}`})
+//   language.value = e.target.value
+// }
 </script>
 <template>
-  <div class="no-print select-language-container">
-    <nav>
-      <label for="languageSelect">Language</label>
-      <select 
-        @input="selectLanguage"
-        v-if="Object.keys(resume_config.languages).length > 1" 
-        v-model="language" 
-        name="languageSelect" 
-        id="languageSelect"   
-      >
-        <option v-for="language of Object.keys(resume_config.languages)" :value="language" :key="language">{{resume_config.languages[language]}}</option>
-      </select>
-    </nav>
-  </div>
+  <!-- <div class="no-print select-language-container"> -->
+  <!--   <nav> -->
+  <!--     <label for="languageSelect">Language</label> -->
+  <!--     <select  -->
+  <!--       @input="selectLanguage" -->
+  <!--       v-if="Object.keys(resume_config.languages).length > 1"  -->
+  <!--       v-model="language"  -->
+  <!--       name="languageSelect"  -->
+  <!--       id="languageSelect"    -->
+  <!--     > -->
+  <!--       <option v-for="language of Object.keys(resume_config.languages)" :value="language" :key="language">{{resume_config.languages[language]}}</option> -->
+  <!--     </select> -->
+  <!--   </nav> -->
+  <!-- </div> -->
   <slot></slot>
 </template>
